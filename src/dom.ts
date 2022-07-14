7fa313eeca56c51eee7bba04bbcf7379779c5de0
alias-rahil <rahil@dyte.in>
2022-07-14 23:48:57 +0530

    fix: source for continuous media streams

--- src/dom.ts
+++ src/dom.ts
@@ -1,28 +1,35 @@
 declare global {
     interface HTMLMediaElement {
-        setSinkId(sinkId: string): Promise<void>; // TODO(alias-rahil): remove later after it's added in `lib.dom.d.ts`.
-        captureStream(): MediaStream; // TODO(alias-rahil): remove later after it's added in `lib.dom.d.ts`.
+        setSinkId(sinkId: string): Promise<void>;
     }
 
     interface MediaDevices {
+        emulatedDevices?: (MediaDeviceInfo | InputDeviceInfo)[];
+
         getDisplayMedia(
             constraints?: DisplayMediaStreamConstraints | EmulatedMediaStreamConstraints,
         ): Promise<MediaStream>;
+
         getUserMedia(
             constraints?: MediaStreamConstraints | EmulatedMediaStreamConstraints,
         ): Promise<MediaStream>;
+
+        addEmulatedDevice(kind: 'audiooutput'): string;
         addEmulatedDevice(
             kind: Exclude<MediaDeviceKind, 'audiooutput'>,
-            capabilities: Omit<
+            capabilities?: Omit<
                 MediaTrackCapabilities & MediaTrackConstraints,
                 'deviceId' | 'groupId'
             >,
         ): string;
-        addEmulatedDevice(kind: 'audiooutput'): string;
-        removeEmulatedDevice(emulatorDeviceId: string): undefined | MediaDeviceInfo;
+
+        removeEmulatedDevice(
+            emulatorDeviceId: string,
+        ): undefined | MediaDeviceInfo | InputDeviceInfo;
+
         silenceTrack(emulatorDeviceId: string): void;
         brickDevice(emulatorDeviceId: string): void;
-        emulatedDevices?: MediaDeviceInfo[];
+        enumerateDevices(): Promise<(MediaDeviceInfo | InputDeviceInfo)[]>;
     }
 
     interface InputDeviceInfo {
@@ -33,9 +40,11 @@ declare global {
         audio?: {
             deviceId: { exact: string };
         };
+
         video?: {
             deviceId: { exact: string };
         };
+
         emulated: true;
     }
 }
